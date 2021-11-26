677ef61718fb33e567746b9bb965714503be57be
Carol_Inglethe <carolinainglethe@gmail.com>
2021-11-26 18:20:01 -0300

    Refatora Desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,14 +1,26 @@
 db.movies.aggregate([
   {
     $match: {
-      countries: "USA",
-      "tomatoes.viewer.rating": { $gte: 3 },
+      $and: [
+        { countries: "USA" },
+        { "tomatoes.viewer.rating": { $gte: 3 } },
+        { cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] } },
+      ],
     },
   },
   {
-    $addFields: { num_favs: { $sum: "$cast" } },
+    $addFields: { num_favs: { $size: "$cast" } },
   },
   {
     $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
+  },
+  {
+    $project: { _id: 0, title: 1 },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
   },
 ]);
