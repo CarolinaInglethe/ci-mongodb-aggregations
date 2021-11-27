f2ced02d8816d45605195c9b1d3708af527c66ec
Carol_Inglethe <carolinainglethe@gmail.com>
2021-11-26 21:37:48 -0300

    .

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -3,7 +3,7 @@ db.trips.aggregate([
     $match: { birthYear: { $exists: true, $ne: "" } },
   },
   {
-    // agrupa passando por cada um e comparando se é maior ou menor que anterior ou proximo
+    // agrupa passando por cada um (id null)e comparando se é maior ou menor que anterior ou proximo
     // usando $max e $min e retorna novo documento:
     $group: {
       _id: null,
