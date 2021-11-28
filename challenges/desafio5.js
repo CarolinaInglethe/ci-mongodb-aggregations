4d9f2cfa277884dad60a60a6aea09992a6339f43
Carol_Inglethe <carolinainglethe@gmail.com>
2021-11-28 16:25:29 -0300

    refatora desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,6 +2,7 @@ db.movies.aggregate([
   {
     $match: {
       $and: [
+        { countries: { $exists: true } },
         { countries: "USA" },
         { "tomatoes.viewer.rating": { $gte: 3 } },
         { cast: { $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] } },
@@ -9,18 +10,30 @@ db.movies.aggregate([
     },
   },
   {
-    $addFields: { num_favs: { $size: "$cast" } },
+    // salva os favoritos em uma variavel
+    $addFields: {
+      favs: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"],
+    },
+  },
+  {
+    // compara os favoritos do documento com os favoritos da variavel criada e gera uma nova
+    // ($setIntersection);
+    // conta o tamanho do array de favoritos gerado e salva em num_favs ($size);
+    $addFields: {
+      num_favs: { $size: { $setIntersection: ["$cast", "$favs"] } },
+    },
   },
   {
     $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
   },
-  {
-    $project: { _id: 0, title: 1 },
-  },
+  // (skip e limit para pegar o 25° documento que segue os criterios)
   {
     $skip: 24,
   },
   {
     $limit: 1,
+  },
+  {
+    $project: { _id: 0, title: 1 },
   },
 ]);
